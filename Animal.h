55449edd9f891cb8fe391226844289c8190fc5be
Tony George <t.george415@gmail.com>
2015-05-02 11:27:20 -0400

    Commit changes

---
+++ Animal.h
@@ -0,0 +1,13 @@
+//
+//  Animal.h
+//  GitHubDummyUser
+//
+//  Created by Tony George on 5/2/15.
+//  Copyright (c) 2015 Tony George. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Animal : NSObject
+
+@end
---
+++ Animal.m
@@ -0,0 +1,13 @@
+//
+//  Animal.m
+//  GitHubDummyUser
+//
+//  Created by Tony George on 5/2/15.
+//  Copyright (c) 2015 Tony George. All rights reserved.
+//
+
+#import "Animal.h"
+
+@implementation Animal
+
+@end
