d5c0773545745207fccf0bb18c20f227bdeae35a
Tony George <t.george415@gmail.com>
2015-05-02 11:13:43 -0400

    Initial Commit

---
+++ GitHubDummyUser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubDummyUser
+//
+//  Created by Tony George on 5/2/15.
+//  Copyright (c) 2015 Tony George. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitHubDummyUser/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitHubDummyUser
+//
+//  Created by Tony George on 5/2/15.
+//  Copyright (c) 2015 Tony George. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
